02ae0dcf7d4f1d24edfbdfd5cac33ae825e41780
unknown <okoriewilfrid.20201206203@futo.edu.ng>
2024-02-20 11:12:38 +0100

    tiny update

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,20 +1,39 @@
 import H4F from '../assets/Asset-1Hack4Futo.png';
 import { NavLink, Outlet } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const RootLayout = () => {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const toggleIsClicked = () => {
+        setIsClicked(!isClicked)
+    }
+
+    const navToggleIsClicked = () => {
+        setIsClicked(false);
+    }
     return ( 
         <div className="root-layout">
             <header>
                 <nav>
-                    <div className='def-img'>
-                        <img src={H4F} alt="Hack4FUTO" />
+                    <div className='item1'>
+                        <div className='def-img'>
+                            <img src={H4F} alt="Hack4FUTO" />
+                        </div>
+                        <div onClick={toggleIsClicked} className='menu'>
+                            {/* <FontAwesomeIcon icon={faAngleDown} className='dropdown-icon' onClick={toggleIsClicked}/> */}
+                            Menu
+                        </div>
                     </div>
-                    <div>
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="about">About Us</NavLink>
-                        <NavLink to='sponsor-us'>Sponsor Us</NavLink>
-                        <NavLink to="faq">FAQ</NavLink>
-                        <NavLink to="core-team">Core Team</NavLink>
+                    <div className={!isClicked? 'navlinks': 'navlinks mobile'}>
+                        <NavLink to="/" onClick={navToggleIsClicked}>Home</NavLink>
+                        <NavLink to="about" onClick={navToggleIsClicked}>About Us</NavLink>
+                        <NavLink to='sponsor-us' onClick={navToggleIsClicked}>Sponsor Us</NavLink>
+                        <NavLink to="faq" onClick={navToggleIsClicked}>FAQ</NavLink>
+                        <NavLink to="core-team" onClick={navToggleIsClicked}>Core Team</NavLink>
                     </div>
                     <div className='register'>
                         <NavLink to="register">Register</NavLink>
